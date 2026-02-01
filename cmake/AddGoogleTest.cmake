90dac10ceddcc8911c4e4ed5f13bd2bd3e90c9cf
jobala <japhethobalak@gmail.com>
2026-02-01 16:20:45 +0300

    fix build error

--- cmake/AddGoogleTest.cmake
+++ cmake/AddGoogleTest.cmake
@@ -2,9 +2,12 @@ include(FetchContent)
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        v1.15.2
+  GIT_TAG        v1.17.0
+  SYSTEM          
 )
 
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
+target_compile_options(gtest PRIVATE -Wno-character-conversion)
+target_compile_options(gtest_main PRIVATE -Wno-character-conversion)
